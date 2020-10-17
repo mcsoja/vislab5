709e4f7877492039cdf841c4c1da365fcd8a78b1
mcsoja <sojam@bc.edu>
2020-10-17 14:32:40 -0400

    done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // CHART INIT ------------------------------
 
 let type = document.querySelector("#group-by").value;
+let sort = 1 //ascending
+console.log(type, sort)
 
 // create svg with margin convention
 let margin = { top: 40, right: 20, bottom: 40, left: 90 },
@@ -26,7 +28,8 @@ let x = d3
   .range([0, width])
   .paddingInner(0.1);
 
-let y = d3.scaleLinear().range([height, 0]);
+let y = d3.scaleLinear()
+    .range([height, 0]);
 
 let xAxis = d3
   .axisBottom()
@@ -50,28 +53,43 @@ let yAxisGroup = svg.append("g").attr("class", "y-axis axis");
 function update(data, type){
     type = document.querySelector("#group-by").value;
     // update domains
-    console.log("updating")
+    console.log("updating", type)
+    if(sort == 1 && type == "stores"){
+        data = data.sort(function(a,b){
+            return b.stores-a.stores
+        })
+    } else if(sort == 1 && type == "revenue"){
+        data = data.sort(function(a,b){
+            return b.revenue-a.revenue
+        })
+    } else if (sort == 0 && type == "stores"){
+        data = data.sort(function(a,b){
+            return a.stores-b.stores})
+    } else {
+        data = data.sort(function(a,b){
+            return a.revenue-b.revenue}
+        )
+        }
+    console.log(data, "SORTED?")
     x.domain(
         data.map(function(d) {
           return d.company;
         })
       );
-      y.domain([
+    y.domain([
         0,
         d3.max(data, d=>d[type])
       ]);
     // update bars
-    let bar1 = svg
+  /*let bar1 = svg
     .selectAll(".bar1")
     .remove()
     .exit()
-    .data(data);
+    .data(data);*/
 
-  bar1
-    .enter()
-    .append("rect")
-    .attr("class", "bar1")
-    .attr('fill', 'blue')
+  svg.selectAll(".bar1")
+    .transition() 
+    .duration(2000)
     .attr("x", function(d) {
       return x(d.company);
     })
@@ -82,14 +100,23 @@ function update(data, type){
       return height - y(d[type]);
     })
     .attr("width", x.bandwidth())
+    
 
 	// update axes and axis title
     xAxisGroup = svg
     .select(".x-axis")
+    .transition()
+    .duration(1000)
+    .delay(500)
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-    yAxisGroup = svg.select(".y-axis").call(yAxis);
+    yAxisGroup = svg
+    .select(".y-axis")
+    .transition()
+    .duration(1000)
+    .delay(1000)
+    .call(yAxis);
 
     svg.select("text.axis-title").remove();
   svg.append("text")
@@ -102,17 +129,69 @@ function update(data, type){
         if (document.querySelector("#group-by").value == 'stores'){
             return "Stores"
         } else{
-            return "Revenue"
+            return "Billions USD"
         }
+    });
+
+    d3.select(".sort")
+    .on("click", function(d) {
+        console.log("CLICK")
+        if (sort == 1){
+            sort = 0
+        } else {
+            sort = 1
+        }
+        console.log(sort)
     });
 }
 
-// CHART UPDATES ---------------------------
-
-// Loading data
 d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
     console.log(data)
-    update(data, type); // simply call the update function with the supplied date
+    let bar1 = svg
+    .selectAll(".bar1")
+    .remove()
+    .exit()
+    .data(data);
+
+    x.domain(
+        data.map(function(d) {
+          return d.company;
+        })
+      );
+    y.domain([
+        0,
+        d3.max(data, d=>d[type])
+      ]);
+
+    bar1
+    .enter()
+    .append("rect")
+    .attr("class", "bar1")
+    .attr('fill', 'blue')
+    .attr("x", function(d) {
+        return x(d.company);
+      })
+      .attr("y", function(d) {
+        return y(d.stores);
+      })
+      .attr("height", function(d) {
+        return height - y(d.stores);
+      })
+      .attr("width", x.bandwidth())
+
+    update(data, type);
+})
+
+function callUpdate(){
+    d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
+        type = document.querySelector("#group-by").value;
+        console.log(data)
+        update(data, type); // simply call the update function with the supplied date
 });
+}
+
+
+
+
 
 // (Later) Handling the sorting direction change
